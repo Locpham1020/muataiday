0bd8193c8ba10a4a374b17bf4673a0ef9e7c3eb7
Locpham1020 <locpv.1020@gmail.com>
2025-04-25 13:47:37 +0700

    Fix route click

--- api/click.py
+++ api/click.py
@@ -1,20 +1,11 @@
 from flask import Blueprint, request, jsonify
-from utils.airtable import send_to_airtable
-from config import AIRTABLE_TOKEN, AIRTABLE_BASE_ID
 
 click_bp = Blueprint("click", __name__)
-TABLE_NAME = "Goiphubac"  # Tên bảng thật trong Airtable
 
 @click_bp.route("/", methods=["POST"])
 def log_click():
-    try:
-        data = request.get_json()
-        response = send_to_airtable(
-            token=AIRTABLE_TOKEN,
-            base_id=AIRTABLE_BASE_ID,
-            table_name=TABLE_NAME,
-            payload={"fields": data}
-        )
-        return jsonify({"status": "success", "data": response})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    data = request.get_json()
+    return jsonify({
+        "status": "success",
+        "data": data
+    })
